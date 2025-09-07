84c2bcd6d069e4a092883db78978810f24b9273d
eray02dev <egeeray490@gmail.com>
2025-09-07 09:11:34 -0600

    feat: wire celebrate validators for /signup and /signin

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,25 @@
 const router = require("express").Router();
 
 const { createUser, login } = require("../controllers/users");
-const { getItems } = require("../controllers/clothingItems"); // <-- public GET handler
+const { getItems } = require("../controllers/clothingItems");
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 const auth = require("../middlewares/auth");
 const { NOT_FOUND } = require("../utils/errors");
 
-// Public routes
-router.post("/signup", createUser);
-router.post("/signin", login);
-router.get("/items", getItems); // <-- sadece GET /items public
+// ✨ celebrate validatorlar
+const { validateSignup, validateSignin } = require("../utils/validators");
 
-// Protected routes (sıra önemli: public GET üstte olmalı)
+// ---- Public routes ----
+router.post("/signup", validateSignup, createUser);
+router.post("/signin", validateSignin, login);
+router.get("/items", getItems); // sadece GET /items public
+
+// ---- Protected routes ----
 router.use("/items", auth, itemRouter); // POST/DELETE/like/unlike korumalı
 router.use("/users", auth, userRouter);
 
-// 404 fallback
+// ---- 404 fallback ----
 router.use("*", (req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
